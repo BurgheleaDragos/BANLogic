7f3bd50804ac172c31c60076206141db23ea8ff8
BurgheleaDragos <burghelea.dragos@gmail.com>
2017-05-03 20:25:25 +0300

    Update BANLogic.cs

--- Proiect 2/BANLogic.cs
+++ Proiect 2/BANLogic.cs
@@ -1,45 +1,55 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Proiect_2.Logic;
+using Proiect_2.Syntax;
 
 namespace Proiect_2
 {
     public class BanLogic
     {
-        public string ProtocolList { get; set; }
-        private List<BanLogic> _protocolSteps;
+        public List<BaseLogic> ProtocolSteps;
         public BanLogic()
         {
-            _protocolSteps = new List<BanLogic>();
+            ProtocolSteps = new List<BaseLogic>();
         }
-        public BanLogic(string protocolList)
+        public BanLogic(List<BaseLogic> protocolSteps)
         {
-            ProtocolList = protocolList;
-            _protocolSteps = new List<BanLogic>();
-            SplitProtocolSteps();
+            ProtocolSteps = protocolSteps;
         }
 
-        private bool SplitProtocolSteps()
+        public void GenerateKnowledge()
         {
-//            _protocolSteps = ProtocolList.Split('\n').ToList();
-            return true;
-        }
-
-        private bool InterpretProtocolStep(string protocolStep)
-        {
-//            Regex.s
-            return true;
-        }
-        public string IdealizingProtocol()
-        {
-            if (_protocolSteps.Count <= 0)
+            foreach (var protocolStep in ProtocolSteps)
             {
-                SplitProtocolSteps();
+                CheckKnowledge(protocolStep);
             }
-            return string.Join(",",_protocolSteps);
+        }
+
+        private void CheckKnowledge(BaseLogic protocolStep)
+        {
+            foreach (var baseLogic in ProtocolSteps)
+            {
+
+                #region ReceiveRule
+                ReceiveRule receiveRule = new ReceiveRule(protocolStep, baseLogic);
+                BaseLogic receiveRuleResult = receiveRule.Result;
+                if (receiveRuleResult != null)
+                {
+                    ProtocolSteps.Add(receiveRuleResult);
+                }
+                ReceiveRule receiveRule2 = new ReceiveRule(baseLogic, protocolStep);
+                BaseLogic receiveRule2Result = receiveRule2.Result;
+                if (receiveRule2Result != null)
+                {
+                    ProtocolSteps.Add(receiveRule2Result);
+                }
+                #endregion
+
+            }
         }
     }
 }
