3d5ec464b5e0c3bb86e1e56db84113cd10c58e95
Oana\-Oana- <oana.ifrim10@yahoo.com>
2017-05-06 17:40:53 +0300

    implemented Kerberos rules

--- Proiect 2/Syntax/Concatenation.cs
+++
@@ -1,9 +0,0 @@
-namespace Proiect_2.Syntax
-{
-    public class Concatenation : BaseLogic
-    {
-        public string X { get; set; }
-        public string Y { get; set; }
-        public string Concatenated => string.Concat(X, Y);
-    }
-}
