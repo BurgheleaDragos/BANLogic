52c3844209e162d535982a7b4c8492c247ecd456
BurgheleaDragos <burghelea.dragos@gmail.com>
2017-05-06 14:53:51 +0300

    added concatenate class. added formula and formula key in encryption class.

---
+++ Proiect 2/Syntax/Concatenate.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Proiect_2.Syntax
+{
+    public class Concatenate : BaseLogic
+    {
+        public List<BaseLogic> Formulas { get; set; }
+
+    }
+}
--- Proiect 2/Syntax/Encryption.cs
+++ Proiect 2/Syntax/Encryption.cs
@@ -2,7 +2,9 @@ namespace Proiect_2.Syntax
 {
     public class Encryption : BaseLogic
     {
+        public BaseLogic Formula { get; set; }
         public string Value { get; set; }
         public string Key { get; set; }
+        public SharedKey FormulaKey { get; set; }
     }
 }
