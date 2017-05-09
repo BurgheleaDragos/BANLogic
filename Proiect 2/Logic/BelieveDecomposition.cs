b7e8483b5fae1b97dd12b263f008fef17a89dd20
BurgheleaDragos <burghelea.dragos@gmail.com>
2017-05-09 22:20:18 +0300

    Oana - added believe composition and decomposition rules.

---
+++ Proiect 2/Logic/BelieveDecomposition.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Proiect_2.Syntax;
+
+namespace Proiect_2.Logic
+{
+    public class BelieveDecomposition
+    {
+        public BaseLogic Formula1 { get; set; }
+        public List<BaseLogic> Result => RuleLogic(Formula1);
+        public BelieveDecomposition() { }
+        public static List<BaseLogic> GetResult(BaseLogic formula1)
+        {
+            return RuleLogic(formula1);
+        }
+
+        public BelieveDecomposition(BaseLogic formula1)
+        {
+            Formula1 = formula1;
+        }
+
+        private static List<BaseLogic> RuleLogic(BaseLogic _formula1)
+        {
+            try
+            {
+                var formula1 = _formula1 as Believe;
+
+                if (formula1 != null &&
+                    formula1.Formula.GetType() == typeof(Concatenate))
+                {
+                    var formula2 = formula1.Formula as Said;
+                    if (formula2 != null &&
+                        formula2.Formula.GetType() == typeof(Concatenate))
+                    {
+                        var concatenate = formula2.Formula as Concatenate;
+                        if (concatenate == null)
+                        {
+                            return null;
+                        }
+                        var ruleLogic = new List<BaseLogic>();
+                        foreach (var formula in concatenate.Formulas)
+                        {
+                            ruleLogic.Add(new Believe()
+                            {
+                                Agent1 = formula1.Agent1,
+                                Formula =formula
+                            });
+                        }
+                        return ruleLogic;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return null;
+        }
+    }
+}
---
+++ Proiect 2/Syntax/From.cs
@@ -0,0 +1,15 @@
+﻿using Proiect_2.Elements;
+
+namespace Proiect_2.Syntax
+{
+    public class From : BaseLogic
+    {
+        public Agent Agent { get; set; }
+        public BaseLogic Formula { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Formula} from {Agent}";
+        }
+    }
+}
